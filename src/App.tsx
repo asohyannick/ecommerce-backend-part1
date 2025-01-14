9dc05792534ea42402734828d7a4d88dd65c0a0a
asohyannick <asohyannick3@gmail.com>
2025-01-13 16:26:51 -0800

    completed my UI setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>React e-commerce web application</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server:{
+    port:3000,
+    proxy: {
+      '/api' : {
+        target: 'http://localhost:8000',
+        secure: false
+      }
+    }
+  },
+  plugins: [react()],
+})
