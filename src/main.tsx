0c5f2e2a55213049b2d6e9d73341116911aba908
asohyannick <asohyannick3@gmail.com>
2025-02-27 23:30:04 -0800

    Remove a single package from the UI

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,8 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { store, persistor } from './store/store/store.tsx';
 import './index.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
-if (process.env.NODE_ENV === 'production') disableReactDevTools();
+if (process.env.NODE_ENV as string === 'production') disableReactDevTools();
 
 createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
